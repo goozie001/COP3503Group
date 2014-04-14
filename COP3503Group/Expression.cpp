b132d5f8f17978ee2a8cd429cc5f29a94be465c7
DylanRichardson <dylanrichardson47@gmail.com>
2014-04-14 16:43:41 -0400

    Expression class implemented.

--- COP3503Group/Expression.cpp
+++ COP3503Group/Expression.cpp
@@ -1,11 +1,46 @@
 #include "Expression.h"
 
-
 Expression::Expression()
 {
 }
 
+Expression::Expression(Number *numb1, Number *numb2, Operator *oper)
+{
+	exprVector[0] = numb1;
+	exprVector[1] = numb2;
+	exprVector[2] = oper;
 
+}
 Expression::~Expression()
 {
+	for (int i = 0; i < exprVector.size(); i++)
+	{
+		delete exprVector.at(i);
+	}
+	// delete[] exprVector;
+}
+
+string Expression::toString()
+{
+	string temp;
+	for (int i = 0; i < exprVector.size(); i++)
+	{
+		temp += exprVector.at(i)->toString();
+	}
+	return temp;
+}
+
+float Expression::getFloatValue()
+{
+	float temp = 0;
+	for (int i = 0; i < exprVector.size(); i++)
+	{
+		temp += exprVector.at(i)->getFloatValue();
+	}
+	return temp;
+}
+
+vector<Number*> Expression::getVector()
+{
+	return exprVector;
 }
--- COP3503Group/Expression.h
+++ COP3503Group/Expression.h
@@ -1,10 +1,18 @@
 #pragma once
 #include "Number.h"
+#include "Operator.h"
 class Expression :
 	public Number
 {
 public:
 	Expression();
+	Expression(Number*, Number*, Operator*);
 	~Expression();
-};
 
+	string toString();
+	float getFloatValue();
+	vector<Number*> getVector();
+private:
+	vector<Number*> exprVector;
+
+};
