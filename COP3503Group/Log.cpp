77247c21471b2b54c2ad1fb9dd858b19c21ae67e
DylanRichardson <dylanrichardson47@gmail.com>
2014-04-22 11:17:38 -0400

    Finished integers for log class.

--- COP3503Group/Log.cpp
+++ COP3503Group/Log.cpp
@@ -37,11 +37,11 @@ Number *Log::simplify() {
 		if (dynamic_cast<Integer*>(argument) != 0) {
 			Integer *baseInt = dynamic_cast<Integer*>(base);
 			Integer *argInt = dynamic_cast<Integer*>(argument);
-			int a = baseInt->getIntValue();
-			int b = argInt->getIntValue();
+			int base = baseInt->getIntValue();
+			int argument = argInt->getIntValue();
 			int c = 1;
 			int d = 0;
-			while (d <= b) {
+		/*	while (d <= b) {
 				d = pow(a, c);
 				if (d == b) {
 					return new Integer(c);
@@ -49,15 +49,49 @@ Number *Log::simplify() {
 				else {
 					++c;
 				}
+			}*/
+			// Change of base to evaluate double value of the log.
+			double result = log((double)argument) / log((double)base);
+			double intpart;
+			if (modf(result, &intpart) == 0.0) {
+				return new Integer((int)intpart);
 			}
-			a = baseInt->getIntValue();
-			int divisor = smallestDivisor(b);
-			if (divisor == b) {
+			// Check for common roots of base that equal the argument.
+			else if (modf(1 / result, &intpart) == 0.0) {
+				return new Expression(new Integer(1), new Integer((int)intpart), new Operator("/"));
+			}
+			// Check all other fractal representations of logarithms.
+			else {
+				double rootAns = 2;
+				int i = 2;
+				while (rootAns >= 2) {
+					rootAns = pow(base, 1 / (double)i);
+					if (modf(rootAns, &intpart) == 0.0 || modf(rootAns, &intpart) > 0.9999999999) {
+						double expAns = 0;
+						int j = 2;
+						while (expAns <= argument) {
+							expAns = pow(rootAns, j);
+							if (expAns == argument || (modf(expAns, &intpart) > 0.9999999999 && expAns < argument && expAns + .00001 > argument)) {
+								return new Expression(new Integer(j), new Integer(i), new Operator("/"));
+							}
+							else {
+								++j;
+							}
+						}
+					}
+					++i;
+				}
+			}
+			
+			// Simplify logs that aren't rational to logs containing smaller arguments.
+			base = baseInt->getIntValue();
+			int divisor = smallestDivisor(argument);
+			if (divisor == argument) {
 				return new Log(baseInt, argInt);
 			}
 			else {
 				Log *newLog1 = new Log(baseInt, new Integer(divisor));
-				Log *newLog2 = new Log(baseInt, new Integer(b / divisor));
+				Log *newLog2 = new Log(baseInt, new Integer(argument / divisor));
 				Operator *plus = new Operator("+");
 				Expression *newEx = new Expression(newLog1, newLog2, plus);
 				return newEx;
